64001a81ca3a3c2328ff46f3c05b5b4f4fe2f2d2
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2019-03-05 15:04:55 -0300

    Lendo arquivos de texto

---
+++ csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,36 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ByteBankImportacaoExportacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arquivo = "contas.txt";
+            var fluxoArquivo = new FileStream(arquivo, FileMode.Open);
+            var buffer = new byte[1024];
+            fluxoArquivo.Read(buffer, 0, 1024);
+            var aQuantidadeByte = -1;
+
+            while (aQuantidadeByte != 0)
+            {
+                aQuantidadeByte = fluxoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void EscreverBuffer(byte[] pBuffer)
+        {
+            var enconding = new UTF8Encoding();
+            Console.Write(enconding.GetString(pBuffer));
+        }
+    }
+}
